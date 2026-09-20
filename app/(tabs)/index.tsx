a1dd3db38065641107f89210183a7e118a5aeb8c
dzzJulian <dalmazzo912@gmail.com>
2026-09-20 04:03:30 -0300

    feat: agregado buscador/search

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,7 @@
-import { Text, View,  StyleSheet } from 'react-native';
+import BookSearch from "../../components/BookSearch"
 
-export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-    </View>
-  );
+const Index = () => {
+    return <BookSearch />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-});
+export default Index
